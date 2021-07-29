15c55202a4579015b104169289e492de04ebf1c5
lucacan <lucacan@picenum.net>
2021-07-29 12:14:47 +0200

    Update beforeend.sh
    Rsync only in overlay mode

--- beforeend.sh
+++ beforeend.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-MOUNT=/usr/bin/mount
-DATE=/usr/bin/date
-RSYNC=/usr/bin/rsync
-ECHO=/usr/bin/echo
 #
-$MOUNT -o remount,rw /ovl/ro
-if [ $? -ne 0 ]; then
-	$ECHO "ERROR: could not mount /ovl/ro in read/write mode" >> /var/log/beforeend.log
-	exit 1
-else    
-	$DATE>>/root/beforeend.log  
-	LIST="bin etc home lib media opt root sbin  srv usr var "
-	for DIRECTORY in $LIST; do
-		$ECHO "Doing directory $DIRECTORY " >> /var/log/beforeend.log
-		$RSYNC -avx --delete  /$DIRECTORY/ /ovl/ro/$DIRECTORY/  >> /var/log/beforeend.log
-		if [ $? -ne 0 ]; then
-			$ECHO "ERROR: could not rsync /ovl/ro/$DIRECTORY " >> /var/log/beforeend.log
-		else
-			$ECHO "Done directory $DIRECTORY" >> /var/log/beforeend.log
-		fi
-	done
+TESTOVL=$(mount | grep "overlayfs-root" )
+if [ -z "$TESTOVL" ]; then
+  exit 0
+else
+  /usr/bin/mount -o remount,rw /ovl/ro
+  if [ $? -ne 0 ]; then
+    /usr/bin/echo "ERROR: could not mount /ovl/ro in read/write mode" >> /var/log/beforeend.log
+    exit 1
+  else    
+    /usr/bin/date>>/root/beforeend.log  
+    LIST="bin etc home lib media opt root sbin  srv usr var "
+    for DIRECTORY in $LIST; do
+      /usr/bin/echo "Doing directory $DIRECTORY " >> /var/log/beforeend.log
+      /usr/bin/rsync -avx --delete  /$DIRECTORY/ /ovl/ro/$DIRECTORY/  >> /var/log/beforeend.log
+      if [ $? -ne 0 ]; then
+        /usr/bin/echo "ERROR: could not rsync /ovl/ro/$DIRECTORY " >> /var/log/beforeend.log
+      else
+        /usr/bin/echo "Done directory $DIRECTORY" >> /var/log/beforeend.log
+      fi
+    done
+  fi
 fi
-$DATE>>/var/log/beforeend.log
-
+/usr/bin/date>>/var/log/beforeend.log
